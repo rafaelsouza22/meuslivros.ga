88a44a2de39ee9949e0da7819c580107ca063df0
Rafael Souza <38994312+rafaelsouza22@users.noreply.github.com>
2021-06-12 00:34:44 -0300

    melhorando o update

--- atualizar.php
+++ atualizar.php
@@ -9,9 +9,11 @@ require_once("./class/SelecionarLivroPorId.php");
 require_once("./class/Conexao.php");
 $atualizar = new atualizarLivro();
 $buscar = new BuscarLivro();
-$selecionar = new SelecionarLivroPorId(); 
-
+$selecionar = new SelecionarLivroPorId();
+$texto = '';
 $livros = array();
+$erros = '';
+$_SESSION['buscar'] = '';
 
 if ((isset($_GET['id_livro']) && !empty($_GET['id_livro']))) {
     $id_livro = addslashes($_GET['id_livro']);
@@ -21,15 +23,15 @@ if ((isset($_GET['id_livro']) && !empty($_GET['id_livro']))) {
 
 if ((isset($_GET['buscar']) && !empty($_GET['buscar']))) {
     $texto = addslashes($_GET['buscar']);
+    $_SESSION['buscar'] = $texto;
     $livrosBuscados = $buscar->buscarLivro($texto);
 }
 
 
 
-
-
-if (isset($_POST['titulo']) && !empty($_POST['titulo']) ) {
-    if ( 
+// para atualizar o livro
+if (isset($_POST['titulo']) && !empty($_POST['titulo'])) {
+    if (
         isset($_POST['id_livro']) && !empty($_POST['id_livro']) &&
         isset($_POST['titulo']) && !empty($_POST['titulo']) &&
         isset($_POST['descricao']) && !empty($_POST['descricao']) &&
@@ -45,22 +47,22 @@ if (isset($_POST['titulo']) && !empty($_POST['titulo']) ) {
         $categoria = addslashes($_POST['categoria']);
         $livroPdf = $_FILES['livroPdf'];
         $livroCapa = $_FILES['livroCapa'];
-        $livro = array('id_livro'=>$id, 'titulo' => $titulo, 'descricao' => $descricao, 'autor' => $autor, 'categoria' => $categoria);
-        
+        $livro = array('id_livro' => $id, 'titulo' => $titulo, 'descricao' => $descricao, 'autor' => $autor, 'categoria' => $categoria, 'livro_pdf' => $livroPdf, 'livro_capa' => $livroCapa);
+
         echo "<pre>";
         var_dump($livro);
         echo "</pre>";
-        
-        $a = $atualizar->atualizarLivro($livro);
+        $id_livro = addslashes($_GET['id_livro']);
+        $livroSelecionado = $selecionar->selecionarLivroPorId($id_livro);
+        //$a = $atualizar->atualizarLivro($livro);
         if ($a) {
             echo "<script> alert('Atualizado com SUCESSO!')</script>";
-        }else{
-            echo"ERRO AO ATUALIZAR!";
+        } else {
+            $erros = "*ERRO AO ATUALIZAR";
         }
     } else {
         echo "Preencha todos os campos!";
     }
-    
 }
 
 ?>
@@ -74,7 +76,6 @@ if (isset($_POST['titulo']) && !empty($_POST['titulo']) ) {
     <link rel="stylesheet" href="./css/atualizar.css">
     <title>Atualizar livros </title>
 </head>
-
 <body>
     <?php require_once("./templetes/header.php"); ?>
     <main>
@@ -84,26 +85,36 @@ if (isset($_POST['titulo']) && !empty($_POST['titulo']) ) {
                 <button type="submit"><img src="./img/lupa.png" alt=""></button>
             </form>
         </section>
-
-
         <section id="form-atualizar">
             <form action="atualizar.php" method="post" enctype="multipart/form-data">
-                <p><?php   ?></p>
-                <input type="hidden" name="id_livro" id="id_livro" value="<?php if(isset($livroSelecionado)){ echo $livroSelecionado['id_livro'];} ?>">
-                <input type="text" name="titulo" id="titulo" placeholder="Titulo do livro" value="<?php if(isset($livroSelecionado)){ echo $livroSelecionado['titulo_livro'];} ?>">
-                <textarea name="descricao" id="descricao" cols="64" rows="10" placeholder="Descrição do livro"> <?php if(isset($livroSelecionado)){ echo $livroSelecionado['descricao_livro'];} ?></textarea>
-                <input type="text" name="autor" id="autor" placeholder="Nome do Autor" value="<?php if(isset($livroSelecionado)){ echo $livroSelecionado['autor_livro'];} ?>">
+                <p class="erros"><?php echo isset($erros) ? $erros : ''; ?></p>
+                <input type="hidden" name="id_livro" id="id_livro" value="<?php if (isset($livroSelecionado)) {
+                                                                                echo $livroSelecionado['id_livro'];
+                                                                            } ?>">
+                <input type="text" name="titulo" id="titulo" placeholder="Titulo do livro" value="<?php if (isset($livroSelecionado)) {
+                                                                                                        echo $livroSelecionado['titulo_livro'];
+                                                                                                    } ?>">
+                <textarea name="descricao" id="descricao" cols="64" rows="10" placeholder="Descrição do livro"> <?php if (isset($livroSelecionado)) {
+                                                                                                                    echo $livroSelecionado['descricao_livro'];
+                                                                                                                } ?></textarea>
+                <input type="text" name="autor" id="autor" placeholder="Nome do Autor" value="<?php if (isset($livroSelecionado)) {
+                                                                                                    echo $livroSelecionado['autor_livro'];
+                                                                                                } ?>">
                 <select name="categoria" id="categoria">
-                    <?php if(isset($livroSelecionado)){ echo "<option value='{$livroSelecionado['categoria_livro']}' > {$livroSelecionado['categoria_livro']} </option> ";} ?>
+                    <?php if (isset($livroSelecionado)) {
+                        echo "<option value='{$livroSelecionado['categoria_livro']}' > {$livroSelecionado['categoria_livro']} </option> ";
+                    } ?>
                     <option value="tecnologia">Tecnologia</option>
                     <option value="finanças">Finanças</option>
                     <option value="aventura">Aventura</option>
-                    <option value="autoajuda" aria-checked="">Auto ajuda</option>
+                    <option value="autoajuda">Auto ajuda</option>
                     <option value="filosofia">Filosofia</option>
                 </select>
                 <fieldset>
                     <legend><label for="livroPdf">Selecionar um Livro *Somente .PDF</label></legend>
-                    <input type="file" name="livroPdf" id="livroPdf" placeholder="Selecione um Livro" value="<?php if(isset($livroSelecionado)){ echo $livroSelecionado['url_pdf_livro'];} ?>">
+                    <input type="file" name="livroPdf" id="livroPdf" placeholder="Selecione um Livro" value="<?php if (isset($livroSelecionado)) {
+                                                                                                                    echo $livroSelecionado['url_pdf_livro'];
+                                                                                                                } ?>">
                 </fieldset>
                 <fieldset>
                     <legend><label for="livroCapa">Selecione uma Capa *Somente .JPG </label></legend>
@@ -116,43 +127,40 @@ if (isset($_POST['titulo']) && !empty($_POST['titulo']) ) {
         <section id="lista-livros">
             <ul>
                 <?php
-                
-                
-
-                    if (empty($livrosBuscados)) {
-                        //echo "Nenhum Livro Achado com o Nome: $texto ";
-                    } else {
-                        echo "<p>Livros Achados por: $texto</p>";
-                        for ($i = 0; $i < count($livrosBuscados); $i++) {
-                            foreach ($livrosBuscados[$i] as $value) {
-                                echo
-                                "<li>
-                                    <a href='atualizar.php?id_livro={$value['id_livro']}'>
-                                        <img src='./arquivos/capas/{$value['url_capa_livro']}' alt='{$value['titulo_livro']}'>
-                                    </a>    
-                                    <h3><a href='atualizar.php?id_livro={$value['id_livro']}'>{$value['titulo_livro']}</a></h3>
-                                    <p> <a href='atualizar.php?id_livro={$value['id_livro']}'>Editar ou Deletar</a></p>
-                                </li>";
-                            }
+                echo (!empty($_SESSION['buscar'])) ? "<p>Livros Achados por: ".$_SESSION['buscar']."</p>" : ''; 
+                if (!empty($livrosBuscados)) {
+                    //echo "<p>Livros Achados por: $texto</p>";
+                    for ($i = 0; $i < count($livrosBuscados); $i++) {
+                        foreach ($livrosBuscados[$i] as $value) {
+                            echo
+                            "<li>
+                                <a href='atualizar.php?id_livro={$value['id_livro']}'>
+                                    <img src='./arquivos/capas/{$value['url_capa_livro']}' alt='{$value['titulo_livro']}'>
+                                </a>    
+                                <h3><a href='atualizar.php?id_livro={$value['id_livro']}'>{$value['titulo_livro']}</a></h3>
+                                <p> <a href='atualizar.php?id_livro={$value['id_livro']}'>Editar ou Deletar</a></p>
+                            </li>";
                         }
-                    } 
-               
+                    }
+                } elseif (!empty($livroSelecionado)) {
+                    //echo "<p>Livros Achados por: $texto</p>";
+                    echo "<li>
+                            <a href='atualizar.php?id_livro={$livroSelecionado['id_livro']}'>
+                                <img src='./arquivos/capas/{$livroSelecionado['url_capa_livro']}' alt='{$livroSelecionado['titulo_livro']}'>
+                            </a>    
+                            <h3><a href='atualizar.php?id_livro={$livroSelecionado['id_livro']}'>{$livroSelecionado['titulo_livro']}</a></h3>
+                            <p> <a href='atualizar.php?id_livro={$livroSelecionado['id_livro']}'>Editar ou Deletar</a></p>
+                        </li>";
+                } else {
+                    echo "Nenhum Livro Achado com o Nome: $texto ";
+                }
                 ?>
             </ul>
         </section>
 
-        
+
     </main>
     <?php require_once("./templetes/footer.php"); ?>
-    <p>BUSCADOS</p>
-    <pre>
-        <?php var_dump($livros);  ?>
-    </pre>
-    <p>SELECIONADO</p>
-    <pre>
-        
-    </pre>
-    
 </body>
 
 </html>
--- class/AtualizarLivro.php
+++ class/AtualizarLivro.php
@@ -7,7 +7,6 @@ class AtualizarLivro extends Conexao
     {
         try {
             $pdo = parent::conexao();
-
             // SETANDO DADOS RECEBIDOS DO LIVRO    
             $id = addslashes($livro['id_livro']);
             $titulo = addslashes($livro['titulo']);
@@ -17,8 +16,19 @@ class AtualizarLivro extends Conexao
             $livroPdf = addslashes($livro['livroPdf']);
             $livroCapa = addslashes($livro['livroCapa']);
             $erros = array();
-             
-            
+
+
+            // APAGANDO O LIVRO E CAPA
+
+            $sql = "SELECT * FROM livros WHERE id_livro = '5' ";
+            $res = $pdo->query($sql);
+            $dados = $res->fetchObject();
+
+
+
+
+
+
             // MOVENDO O LIVRO NOVO
             if (!empty($livro['livroPdf']['name']) && !empty($livro['livroCapa']['name'])) {
                 $nomeLivro = explode('.', $livro['livroPdf']['name']);
@@ -37,10 +47,10 @@ class AtualizarLivro extends Conexao
                 return $erros;
             }
 
-        
+
             // ATUALIZANDO DADOS DO LIVRO
-            $sql = "UPDATE livros SET titulo_livro = :titulo, descricao_livro = :descricao,autor_livro = :autor,
-                    categoria_livro = :categoria , url_pdf_livro = :url_pdf , url_capa_livro = :url_capa WHERE id_livro = :id ";
+            /*$sql = "UPDATE livros SET titulo_livro = :titulo, descricao_livro = :descricao,autor_livro = :autor,
+                categoria_livro = :categoria , url_pdf_livro = :url_pdf , url_capa_livro = :url_capa WHERE id_livro = :id ";
             $con = $pdo->prepare($sql);
             $con->bindValue(':id',  $id);
             $con->bindValue(':titulo', $titulo);
@@ -57,44 +67,32 @@ class AtualizarLivro extends Conexao
             } else {
                 return false;
             }
+            */
         } catch (PDOException $e) {
             echo "Erro no DB: {$e->getMessage()}";
         } catch (Exception $e) {
             echo "Erro: {$e->getMessage()}";
         }
 
+        /*-------------------------------  */
 
-        echo "<pre>";
-        var_dump($livro);
-        echo "</pre>";
-
-
-
-
-        /*------------------------------- 
-
-            $erros = array();
-            //$urlPdf = $urlCapa = '';
-            if (!empty($livro['livroPdf']['name']) && !empty($livro['livroCapa']['name'])) {
-                $nomeLivro = explode('.', $livro['livroPdf']['name']);
-                $nomeCapa = explode('.', $livro['livroCapa']['name']);
-                if (($nomeLivro[sizeof($nomeLivro) - 1] == 'pdf') && ($nomeCapa[sizeof($nomeCapa) - 1] == 'jpg')) {
-                    $urlPdf = sha1($nomeLivro[0]) . '.' . $nomeLivro[sizeof($nomeLivro) - 1];
-                    move_uploaded_file($livro['livroPdf']['tmp_name'], "./arquivos/livros/$urlPdf");
-                    $urlCapa = sha1($nomeCapa[0]) . '.' . $nomeCapa[sizeof($nomeCapa) - 1];
-                    move_uploaded_file($livro['livroCapa']['tmp_name'], "./arquivos/capas/$urlCapa");
-                } else {
-                    $erros = 'Você não pode fazer upload deste tipo de arquivo';
-                    return $erros;
-                }
+        if (!empty($livro['livroPdf']['name']) && !empty($livro['livroCapa']['name'])) {
+            $nomeLivro = explode('.', $livro['livroPdf']['name']);
+            $nomeCapa = explode('.', $livro['livroCapa']['name']);
+            if (($nomeLivro[sizeof($nomeLivro) - 1] == 'pdf') && ($nomeCapa[sizeof($nomeCapa) - 1] == 'jpg')) {
+                $urlPdf = sha1($nomeLivro[0]) . '.' . $nomeLivro[sizeof($nomeLivro) - 1];
+                move_uploaded_file($livro['livroPdf']['tmp_name'], "./arquivos/livros/$urlPdf");
+                $urlCapa = sha1($nomeCapa[0]) . '.' . $nomeCapa[sizeof($nomeCapa) - 1];
+                move_uploaded_file($livro['livroCapa']['tmp_name'], "./arquivos/capas/$urlCapa");
             } else {
-                $erros = 'Escolha um LIVRO e UMA CAPA';
+                $erros = 'Você não pode fazer upload deste tipo de arquivo';
                 return $erros;
             }
-
-            date_default_timezone_set('America/Sao_Paulo');
-            $dataPostagem = date('Y-m-d H:i:s');
-            return true;
-        */
+        } else {
+            $erros = 'Escolha um LIVRO e UMA CAPA';
+            return $erros;
+        }
+        return true;
     }
 }
+
