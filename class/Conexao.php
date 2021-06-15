cc95b2cc67b7edce61c1ba5a3abf45bf6825bc7e
Rafael Souza <38994312+rafaelsouza22@users.noreply.github.com>
2021-06-14 23:20:33 -0300

    Update Conexao.php

--- class/Conexao.php
+++ class/Conexao.php
@@ -1,25 +1,17 @@
 <?php
 
- class Conexao{
+abstract class Conexao{
     
     private const HOST = "127.0.0.1:3306";
     private const DBNAME = "meuslivros";
     private const USER = "root";
     private const PASS = "";
     protected $pdo;
-    public function conexao()
+    protected function conexao()
     {
         try{
             $this->pdo = new PDO('mysql:host=127.0.0.1:3306;dbname=meuslivros','root', '');
-            //$this->pdo = new PDO('mysql:host='.self::HOST.';dbname='.self::DBNAME ,self::USER, self::PASS);
-            // if($this->pdo){
-            //     echo "ok";
-            // }else{
-            //     echo "ERRO";
-            // }
             return $this->pdo;
-            
-
         }catch(PDOException $e){
             echo"Erro no DB: {$e->getMessage()}";
         }catch(Exception $e){
@@ -27,7 +19,6 @@
         }
     }
 }
-// $con = new Conexao();
-// var_dump($con->conexao());
+
 
    
