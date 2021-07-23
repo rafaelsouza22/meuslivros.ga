ae4b22a54a299af3ecdc1811b4c25313a5e99c6a
Rafael Souza <38994312+rafaelsouza22@users.noreply.github.com>
2021-07-23 03:15:02 -0300

    responsividade
    Melhorando a responsividade das paginas livro.php, header.php

--- livro.php
+++ livro.php
@@ -3,6 +3,9 @@ session_start();
 require_once("./Model/SelecionarLivroPorId.php");
 $selecionar = new SelecionarLivroPorId();
 $idLivro = addslashes($_GET['id']);
+if(empty($idLivro)){
+    header('Location: index.php');
+}
 $livro = $selecionar->selecionarLivro($idLivro);
 
 ?>
@@ -23,33 +26,30 @@ $livro = $selecionar->selecionarLivro($idLivro);
 <body>
     <?php require_once("./templetes/header.php"); ?>
     <main>
-        <section class="livro">
+        <section class="livro clear">
             <article>
                 <figure>
                     <img src="./arquivos/capas/<?php echo $livro['url_capa_livro'] ?>" alt="<?php echo $livro['titulo_livro']; ?>">
                 </figure>
-
-                <div id="descicao" >
+                <div id="descicao">
                     <h1><?php echo $livro['titulo_livro']; ?></h1>
-                    <p><strong>Descrição:</strong>  <?php echo $livro['descricao_livro']; ?> </p>
                     <p><strong>Autor:</strong> <?php echo $livro['autor_livro']; ?></p>
                     <p><strong>Categoria:</strong> <?php echo $livro['categoria_livro']; ?></p>
+                    <p><strong>Descrição:</strong> <?php echo $livro['descricao_livro']; ?> </p>
                     <p class="postegem">Postado em <?php echo $livro['data_postagem_livro']; ?></p>
-
                 </div>
             </article>
-            <div class="botoes clear">
-                <p>
-                    <a target="_new" href="./arquivos/livros/<?php echo $livro['url_pdf_livro'] ?>">Ler Livro</a>
-                    <a target="new " href="./arquivos/livros/<?php echo $livro['url_pdf_livro'] ?>">Baixar Livro</a>
-                </p>
+            <div class="botoes">
+                <a target="_new" href="./arquivos/livros/<?php echo $livro['url_pdf_livro'] ?>">Ler Livro</a>
+                <a target="new " href="./arquivos/livros/<?php echo $livro['url_pdf_livro'] ?>">Baixar Livro</a>
             </div>
-            
-            
         </section>
     </main>
     <?php require_once("./templetes/footer.php"); ?>
 </body>
-<!-- <object data="./arquivos/livros/<?php //echo $livro['url_pdf_livro'] 
-                                                    ?>" type="" width="500" height="600" title="ola mundo"></object> -->
+<!--
+    <object data="./arquivos/livros/<?php //echo $livro['url_pdf_livro'] 
+                                    ?>" type="" width="500" height="600" title="ola mundo"></object>
+ -->
+
 </html>
